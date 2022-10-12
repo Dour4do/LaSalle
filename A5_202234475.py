260e70465bd982c84ead468bc41896817499d438
Gabriel <gabrieldourado@hotmail.com>
2022-10-12 10:12:52 -0400

    fixing the if logic

--- A5_202234475.py
+++ A5_202234475.py
@@ -9,11 +9,11 @@ fm = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, \
       31]
 
 #test to see if the date input (m + d) belongs to the specific season
-if ((m == 12) and (d in fm[20:32])) or (m == (1 or 2)) or ((m == 3) and (d in fm[0:21])):
+if ((m == 12) and (d in fm[20:32])) or (m == (1) or (2)) or ((m == 3) and (d in fm[0:21])):
     print('WINTER')
-elif ((m == 3) and (d in fm[20:32])) or (m == (4 or 5)) or ((m == 6) and (d in fm[0:21])):
+elif ((m == 3) and (d in fm[20:32])) or (m == (4) or (5)) or ((m == 6) and (d in fm[0:21])):
     print('SPRING')
-elif ((m == 6) and (d in fm[20:32])) or (m == (7 or 8)) or ((m == 9) and (d in fm[0:21])):
+elif ((m == 6) and (d in fm[20:32])) or (m == (7) or (8)) or ((m == 9) and (d in fm[0:21])):
     print('SUMMER')
 else:
     print('AUTUMN')
